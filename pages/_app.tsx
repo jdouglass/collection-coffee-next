0862b0d595b9f0ad737d18090079085186928f94
Johnathon <johnathondouglass@gmail.com>
2022-11-08 21:32:21 -0800

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next';
 import type { AppProps, NextWebVitalsMetric } from 'next/app';
+import Script from 'next/script';
 import { ReactElement, ReactNode } from 'react';
+import * as gtag from '../lib/gtag';
 import '../pages/globals.css';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -15,7 +17,30 @@ type AppPropsWithLayout = AppProps & {
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page): any => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {

537b873bf00f1cff22ff7e7acef2e27473596fe3
Johnathon <johnathondouglass@gmail.com>
2023-01-30 22:11:58 -0800

    fix: remove some comments and add console log

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -10,6 +10,7 @@ async function generateSiteMap() {
     '!pages/api',
     '!pages/404.ts*',
   ]);
+  console.log(pages);
 
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -38,13 +39,9 @@ function SiteMap() {
 }
 
 export async function getServerSideProps({ res }: { res: NextApiResponse }) {
-  // We make an API call to gather the URLs for our site
-
-  // We generate the XML sitemap with the posts data
   const sitemap = await generateSiteMap();
 
   res.setHeader('Content-Type', 'text/xml');
-  // we send the XML to the browser
   res.write(sitemap);
   res.end();
 

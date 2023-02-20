11575c1e89c2b9111e33f2559756b13a600f4baa
Johnathon <johnathondouglass@gmail.com>
2023-02-20 14:07:34 -0800

    fix: remove unused filter disclosure mock options

--- components/filters/disclosure/base/FilterDisclosure.mocks.ts
+++ components/filters/disclosure/base/FilterDisclosure.mocks.ts
@@ -2,18 +2,6 @@ import { IFilterDisclosure } from './FilterDisclosure';
 
 const base: IFilterDisclosure = {
   section: 'Varieties',
-  options: [
-    'Gesha',
-    'Pink Bourbon',
-    'Catuai',
-    'Caturra',
-    'SL28',
-    'SL34',
-    'Typica',
-    'Sumatra',
-    'Ruiru 11',
-    'Ethiopian Landraces',
-  ],
 };
 
 export const mockFilterDisclosureProps = {

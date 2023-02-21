fae518b7de51b9ec969f03f74fd4edf38fda3a1b
Johnathon <johnathondouglass@gmail.com>
2023-02-21 13:06:48 -0800

    fix: add mock data to landing stats

--- components/stats/LandingStats.mocks.ts
+++ components/stats/LandingStats.mocks.ts
@@ -1,6 +1,10 @@
 import { ILandingStats } from './LandingStats';
 
-const base: ILandingStats = {};
+const base: ILandingStats = {
+  totalProducts: 400,
+  totalVendors: 30,
+  totalBrands: 83,
+};
 
 export const mockFilterCategoryProps = {
   base,

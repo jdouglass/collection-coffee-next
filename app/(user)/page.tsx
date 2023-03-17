58f5dec8934d23f19ddf19070364967e81fb1d9d
Johnathon <johnathondouglass@gmail.com>
2023-03-16 18:10:56 -0700

    fix: change count caching and revalidation again

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -9,7 +9,8 @@ import { LandingPageStats } from '../../typings';
 async function getCount(): Promise<LandingPageStats> {
   const API_BASE_URL = process.env.API_BASE_URL!;
   const res = await fetch(`${API_BASE_URL}/api/landingPageStats`, {
-    cache: 'no-cache',
+    cache: 'no-store',
+    next: { revalidate: 900 },
   });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary

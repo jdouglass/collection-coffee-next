e1558b0ab699bef1eb46cc02b5c407bb13f4717d
Johnathon <johnathondouglass@gmail.com>
2023-03-30 22:21:44 -0700

    fix: remove aspect ratio for post list

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -10,12 +10,12 @@ export default function BlogList({ posts }: Props) {
     <div className="px-8 py-5 lg:py-8 mx-auto xl:px-5 max-w-screen-lg">
       <div className="grid gap-10 lg:gap-10 md:grid-cols-2">
         {posts.slice(0, 2).map((post) => (
-          <ResourceCard key={post._id} post={post} aspect={'video'} />
+          <ResourceCard key={post._id} post={post} />
         ))}
       </div>
       <div className="grid gap-10 mt-10 lg:gap-10 md:grid-cols-2 xl:grid-cols-3">
         {posts.slice(2).map((post) => (
-          <ResourceCard key={post._id} post={post} aspect={'square'} />
+          <ResourceCard key={post._id} post={post} />
         ))}
       </div>
     </div>

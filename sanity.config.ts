f22da5c5f0848820e0329d5bb3ac084ec2b1ab47
Johnathon <johnathondouglass@gmail.com>
2023-03-31 19:43:15 -0700

    feat: add sanity media plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
+import { media, mediaAssetSource } from 'sanity-plugin-media';
 import { deskTool } from 'sanity/desk';
 import { defaultDocumentNode } from './lib/deskStructure';
 import { schemaTypes } from './schemas';
@@ -13,15 +14,24 @@ export default defineConfig({
   title: 'Collection Coffee Studio',
   projectId,
   dataset,
-
   plugins: [
     deskTool({
       defaultDocumentNode: defaultDocumentNode,
     }),
     visionTool(),
+    media(),
   ],
-
   schema: {
     types: schemaTypes,
+  },
+  form: {
+    // Don't use this plugin when selecting files only (but allow all other enabled asset sources)
+    file: {
+      assetSources: (previousAssetSources) => {
+        return previousAssetSources.filter(
+          (assetSource) => assetSource !== mediaAssetSource
+        );
+      },
+    },
   },
 });

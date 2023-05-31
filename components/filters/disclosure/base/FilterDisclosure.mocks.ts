8f501298921da04197ae215cde88afebcf26798e
Johnathon <johnathondouglass@gmail.com>
2023-05-31 09:32:40 -0700

    fix: add mocked product counts

--- components/filters/disclosure/base/FilterDisclosure.mocks.ts
+++ components/filters/disclosure/base/FilterDisclosure.mocks.ts
@@ -2,6 +2,16 @@ import { IFilterDisclosure } from './FilterDisclosure';
 
 const base: IFilterDisclosure = {
   section: 'Varieties',
+  productCounts: {
+    total: 100,
+    roaster: {},
+    vendor: {},
+    country: {},
+    process_category: {},
+    vendor_location: {},
+    tasting_notes: {},
+    variety: {},
+  },
 };
 
 export const mockFilterDisclosureProps = {

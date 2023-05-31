44155d779e3604cbd6a3d744722305306da90ada
Johnathon <johnathondouglass@gmail.com>
2023-05-31 09:33:55 -0700

    fix: add mocked product counts for filter bar

--- components/utility/filter-bar/FilterBar.mocks.ts
+++ components/utility/filter-bar/FilterBar.mocks.ts
@@ -1,6 +1,17 @@
 import { IFilterBar } from './FilterBar';
 
-const base: IFilterBar = {};
+const base: IFilterBar = {
+  productCounts: {
+    total: 100,
+    roaster: {},
+    vendor: {},
+    country: {},
+    process_category: {},
+    vendor_location: {},
+    tasting_notes: {},
+    variety: {},
+  },
+};
 
 export const mockFilterBarProps = {
   base,
